45b4a859e13a72de26ec21d4ea217f7e74b08abc
GitNiko <galaxis.ling@gmail.com>
2018-11-15 15:33:35 +0800

    dnd

--- editor/pages/editor.js
+++ editor/pages/editor.js
@@ -1,4 +1,5 @@
 import { useState, useEffect, useRef } from 'react'
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd'
 import { withRouter } from 'next/router'
 import Link from 'next/link'
 import semver from 'semver'
@@ -12,295 +13,155 @@ import './editor.less'
 
 const noop = () => {}
 
-const Block = ({ data = '' }) => {
-  const onDragStart = ev => {
-    ev.dataTransfer.dropEffect = 'copy'
-    ev.dataTransfer.setData('text/plain', data)
-  }
-  return (
-    <div className="file" draggable onDragStart={onDragStart}>
-      {data}
-    </div>
-  )
-}
+const getExtension = str => str.split('.').pop()
+// a*b*c*d ...
+const encode = (...args) => args.reduce((acc, x) => (acc = acc + '*' + x))
+const decode = str => str.split('*')
 
-const getSortable = (handlers = {}) => {
-  const onDragStart = ev => {
-    ev.dataTransfer.dropEffect = 'copy'
-    ev.dataTransfer.setData('text/plain', data)
-  }
-  const onDragOver = ev => {
-    ev.preventDefault()
-    const rect = ev.target.getBoundingClientRect()
-    const middle = rect.height / 2
-    const offset = ev.clientY - rect.top
-    // console.log(offset)
-    if (offset < middle) {
-      onDragDownward(ev)
-    } else {
-      onDragUpward(ev)
-    }
-  }
-  const onDragEnd = ev => {
-    console.log('end')
-  }
-  const onDrop = ev => {
-    console.log('onDrop in Block')
-    ev.stopPropagation()
-  }
-  const onDragDownward = ev => {
-    if (handlers.onDragDownward) {
-      handlers.onDragDownward(ev)
-    }
-  }
-  const onDragUpward = ev => {
-    if (handlers.onDragUpward) {
-      handlers.onDragUpward(ev)
-    }
-  }
-  const draggable = true
-  // remove Unknown event handler property avoid warning
-  let cloneHandlers = { ...handlers }
-  delete cloneHandlers['onDragDownward']
-  delete cloneHandlers['onDragUpward']
-  return {
-    onDragStart,
-    onDragOver,
-    onDrop,
-    draggable,
-    onDragEnd,
-    ...cloneHandlers,
-  }
-}
-
-const SortBlock = ({
-  data = '',
-  onDragStart = noop,
-  onDrop = noop,
-  onDelete = noop,
-}) => {
-  const [toward, setToward] = useState('up')
-  const sortableProps = getSortable({
-    onDragStart: ev => {
-      ev.dataTransfer.dropEffect = 'move'
-      ev.dataTransfer.setData('text/plain', data)
-      onDragStart(data)
-    },
-    onDragDownward: ev => {
-      console.log('down')
-      setToward('down')
-    },
-    onDragUpward: ev => {
-      console.log('up')
-      setToward('up')
-    },
-    onDrop: ev => {
-      onDrop(ev, data, toward)
-      ev.stopPropagation()
-    },
-  })
-
-  const onClick = ev => {
-    onDelete(data)
-  }
-
-  return (
-    <div className="file" {...sortableProps}>
-      <span onClick={onClick}>remove:</span>
-      {data}
-    </div>
-  )
-}
-
-const SourceCard = ({
-  onSourceChange = noop,
-  onSolutionNameChange = noop,
-  source = [],
-  solutionName = '',
-  type,
-  theKey,
-}) => {
-  const onDrop = ev => {
-    const data = ev.dataTransfer.getData('text/plain')
-    if (source.indexOf(data) !== -1) {
-      // skip when exist
-      return
-    }
-    onSourceChange([...source, data], theKey)
-  }
-  const onDragOver = ev => {
-    ev.preventDefault()
-  }
-  const onItemDragStart = data => {
-    // todo
-  }
-  const onDelete = data => {
-    console.log(data)
-  }
-  const onItemDrop = (ev, self, toward) => {
-    const data = ev.dataTransfer.getData('text/plain')
-    if (self === data) {
-      // skip when drop on self
-      return
-    }
-    let index = source.indexOf(self)
-
-    if (index !== -1) {
-      let newSource = source.filter(e => e !== data) // remove origin data
-      index = newSource.indexOf(self)
-      console.log(toward)
-      if (toward === 'up') {
-        newSource.splice(index + 1, 0, data)
-      } else {
-        newSource.splice(index, 0, data)
-      }
-      onSourceChange(newSource, theKey)
-    }
-  }
+const SourceCard = ({ source = [], solutionName = '', type, droppableId, onSolutionNameChange=noop }) => {
+  const [name, setName] = useState(solutionName)
   const onNameChange = ev => {
-    onSolutionNameChange(ev.target.value, theKey)
+    setName(ev.target.value)
+    // onSolutionNameChange(ev.target.value, theKey)
   }
-  const Blocks = source.map((v, i) => {
-    return (
-      <SortBlock
-        key={i}
-        data={v}
-        onDragStart={onItemDragStart}
-        onDrop={onItemDrop}
-        onDelete={onDelete}
-      />
-    )
-  })
   return (
     <div>
       <div>
         <label>Solution Name:</label>
-        <input value={solutionName} onChange={onNameChange} />
+        <input value={name} onChange={onNameChange} onBlur={() => onSolutionNameChange(solutionName, name)}/>
       </div>
-      <div onDrop={onDrop} onDragOver={onDragOver} className="source-card">
-        {Blocks}
-      </div>
+      <Droppable droppableId={droppableId}>
+        {(provided, snapshot) => (
+          <div
+            ref={provided.innerRef}
+            style={getListStyle(snapshot.isDraggingOver)}
+          >
+            {source.map((item, index) => (
+              <Draggable
+                key={item}
+                draggableId={encode(droppableId, item)}
+                index={index}
+              >
+                {(provided, snapshot) => (
+                  <div
+                    ref={provided.innerRef}
+                    {...provided.draggableProps}
+                    {...provided.dragHandleProps}
+                    style={getItemStyle(
+                      snapshot.isDragging,
+                      provided.draggableProps.style,
+                    )}
+                  >
+                    {item}
+                  </div>
+                )}
+              </Draggable>
+            ))}
+            {provided.placeholder}
+          </div>
+        )}
+      </Droppable>
     </div>
   )
 }
 
-// const reducer = (state, action) => {
-//   switch (action.type) {
-//     case 'editor/set_versions':
-//       return {
-//         ...state,
-//         versions: action.versions,
-//       }
-//     case 'editor/set_selected_version':
-//       return {
-//         ...state,
-//         selectedVersion: action.selectedVersion,
-//       }
-//     case 'editor/set_source':
-//       return {
-//         ...state,
-//         source: action.source,
-//       }
-//   }
-//   return state
-// }
+const getListStyle = isDraggingOver => ({
+  background: isDraggingOver ? 'lightblue' : 'lightgrey',
+  padding: grid,
+  width: 250,
+})
 
-// const useEditorStatus = (name, range) => {
-//   const [state, setState] = useState({
-//     versions: [],
-//     selectedVersion: '',
-//     source: [],
-//   })
+const grid = 8
 
-//   const { selectedVersion } = state
+const getItemStyle = (isDragging, draggableStyle) => ({
+  // some basic styles to make the items look a bit nicer
+  userSelect: 'none',
+  padding: grid * 2,
+  margin: `0 0 ${grid}px 0`,
 
-//   const dispatch = action => {
-//     const nextState = reducer(state, action)
-//     setState(nextState)
-//   }
+  // change background colour if dragging
+  background: isDragging ? 'lightgreen' : 'grey',
 
-//   useEffect(
-//     () => {
-//       getPackageInfo(name).then(info => {
-//         const versions = Object.keys(info.versions).filter(e => {
-//           return semver.satisfies(e, range)
-//         })
-//         dispatch({ type: 'editor/set_versions', versions })
-//         // default is the first version
-//         dispatch({ type: 'editor/set_selected_version', selectedVersion })
-//       })
-//     },
-//     [name],
-//   )
+  // styles we need to apply on draggables
+  ...draggableStyle,
+})
 
-//   useEffect(
-//     () => {
-//       if (selectedVersion) {
-//         getPackageMetaList()(name, selectedVersion).then(source => {
-//           dispatch({ type: 'editor/set_source', source })
-//         })
-//       }
-//     },
-//     [state],
-//   )
+function useSolution(
+  defaultSolution = {
+    root: '',
+    js: {
+      development: [],
+      deployment: [],
+    },
+    css: {},
+  },
+) {
+  const [state, setState] = useState(defaultSolution)
 
-//   return [state, dispatch]
-// }
-// const EditorContext = React.createContext({
-//   state: {},
-//   dispatch: noop,
-// })
+  const reOrder = type => (name, src, dest) => {
+    const newState = { ...state }
+    const [removed] = newState[type][name].splice(src, 1)
+    newState[type][name].splice(dest, 0, removed)
+    setState(newState)
+  }
+  const insert = type => (name, file, index) => {
+    const newState = { ...state }
+    if (!newState[type][name]) {
+      // init and insert first file
+      newState[type][name] = [file]
+    } else {
+      // if exist then insert
+      newState[type][name].splice(index, 0, file)
+    }
+    setState(newState)
+  }
 
-// export default withRouter(({ router }) => {
-//   const { name, range } = router.query
+  const remove = type => (name, file, index) => {
+    const newState = { ...state }
+    if (!newState[type][name]) {
+      // skip
+      return
+    } else {
+      // if exist then delete
+      newState[type][name].splice(index, 1)
+    }
+    setState(newState)
+  }
 
-//   const [state, dispatch] = useEditorStatus(name, range)
+  const create = type => name => {
+    const newState = { ...state }
+    newState[type][name] = []
+    setState(newState)
+  }
 
-//   const onSelect = e => {
-//     setSelectedVersion(e.target.value)
-//     dispatch({type: 'editor/set_selected_version', selectedVersion: e.target.value})
-//   }
+  const del = type => name => {
+    const newState = { ...state }
+    delete newState[type][name]
+    setState(newState)
+  }
 
-//   const {versions, selectedVersion, source} = state
+  const rename = type => (name, newName) => {
+    const newState = { ...state }
+    const origin = newState[type][name]
+    delete newState[type][name]
+    newState[type][newName] = origin
+    setState(newState)
+  }
 
-//   const VersionOptions = versions.map((v, i) => {
-//     return (
-//       <option key={i} value={v}>
-//         {v}
-//       </option>
-//     )
-//   })
-//   const Files = source.map((v, i) => {
-//     return <Block key={i} data={v} />
-//   })
-//   return (
-//     <div className="container">
-//       <div className="source">
-//         <h3>包内容窗口</h3>
-//         <select defaultValue={selectedVersion} onChange={onSelect}>
-//           {VersionOptions}
-//         </select>
-//         <div>{Files}</div>
-//       </div>
-//       <div>
-//         <h3>资源窗口</h3>
-//         <div>
-//           <label>Version Range:</label>
-//           <input />
-//         </div>
-//         <div>
-//           <label>window.</label>
-//           <input />
-//         </div>
-//         <div>
-//           <h4>JavaScript</h4>
-//           <SourceCard source={['/packks']} title="dev" />
-//         </div>
-//       </div>
-//       <div>测试窗口</div>
-//     </div>
-//   )
-// })
+  return [
+    state,
+    {
+      setRoot: root => setState({ ...state, root }),
+      insert,
+      remove,
+      create,
+      del,
+      rename,
+      reOrder,
+    },
+  ]
+}
+
 export default withRouter(({ router }) => {
   const { name, range } = router.query
 
@@ -328,7 +189,14 @@ export default withRouter(({ router }) => {
     () => {
       if (selectedVersion) {
         getPackageMetaList()(name, selectedVersion).then(list => {
-          return setSource(list.map(e => e.path))
+          return setSource(
+            list
+              .map(e => e.path)
+              .filter(
+                path =>
+                  getExtension(path) === 'js' || getExtension(path) === 'css',
+              ),
+          )
         })
       }
     },
@@ -347,68 +215,142 @@ export default withRouter(({ router }) => {
       </option>
     )
   })
-  const Files = source.map((v, i) => {
-    return <Block key={i} data={v} />
-  })
-
-  const [jsSolutions, setJSSolutions] = useState([
-    {
-      solutionName: '',
-      source: [],
-    },
-  ])
-  const [cssSolutions, setCSSSolutions] = useState([{
-    solutionName: '',
-    source: [],
-  }])
-
-  const onSolutionNameChangeOfJS = (solutionName, i) => {
-    const newSolution = { ...jsSolutions[i], solutionName }
-    jsSolutions[i] = newSolution
-    setJSSolutions(jsSolutions)
+  const [
+    solution,
+    { setRoot, insert, remove, create, del, rename, reOrder },
+  ] = useSolution()
+  const onAddSolution = type => {
+    create(type)()
   }
-  const onSourceChangeOfJS = (source, i) => {
-    const newSolution = { ...jsSolutions[i], source }
-    jsSolutions[i] = newSolution
-    setJSSolutions(jsSolutions)
+  const onSolutionNameChange = (type)=>(name, newName) => {
+    rename(type)(name, newName)
   }
-  const JSSolutions = jsSolutions.map((solution, i) => {
-    return (
-      <div>
-        <h4>JavaScript</h4>
+  const onDragStart = ev => {
+    /*...*/
+  }
+  const onDragUpdate = () => {
+    /*...*/
+  }
+  const onDragEnd = result => {
+    // the only one that is required
+    const { source, destination, draggableId } = result
+    const type = getExtension(draggableId)
+
+    if (type !== 'js' && type !== 'css') {
+      return
+    }
+
+    // dropped outside the list
+    if (!destination) {
+      // remove solution when dropped outside solution
+      if (source.droppableId !== 'source') {
+        const name = decode(source.droppableId)[1]
+        remove(type)(name, decode(draggableId).pop(), source.index)
+      }
+      return
+    }
+    // not need change for source
+    if (destination.droppableId === 'source') {
+      return
+    }
+    // not need change between solutions
+    if (
+      source.droppableId !== 'source' &&
+      source.droppableId !== destination.droppableId
+    ) {
+      return
+    }
+
+    const name = decode(destination.droppableId)[1]
+    if (source.droppableId === destination.droppableId) {
+      reOrder(type)(name, source.index, destination.index)
+    } else {
+      if (source.droppableId === 'source') {
+        // don't insert if exist
+        if (solution[type][name].find(x => x === decode(draggableId).pop())) {
+          return
+        }
+      }
+      insert(type)(name, decode(draggableId).pop(), destination.index)
+    }
+  }
+  const getSolution = type => sln =>
+    Object.keys(sln[type]).map((solutionName, i) => {
+      const source = sln[type][solutionName]
+      const droppableId = encode(type, solutionName)
+      return (
         <SourceCard
-          key = {i}
-          theKey = {i}
-          onSolutionNameChange={onSolutionNameChangeOfJS}
-          onSourceChange={onSourceChangeOfJS}
-          source={solution.source}
-          solutionName={solution.solutionName}
+          key={i}
+          source={source}
+          solutionName={solutionName}
+          // onSolutionNameChange={onSolutionNameChange(type)}
+          droppableId={droppableId}
         />
-      </div>
-    )
-  })
+      )
+    })
+  const JSSolutions = getSolution('js')(solution)
+  const CSSSolutions = getSolution('css')(solution)
 
   return (
     <div className="container">
-      <div className="source">
-        <h3>包内容窗口</h3>
-        <select defaultValue={selectedVersion} onChange={onSelect}>
-          {VersionOptions}
-        </select>
-        <div>{Files}</div>
-      </div>
-      <div>
-        <h3>资源窗口</h3>
-        <div>
-          <label>Version Range:</label>
-          <input />
+      <DragDropContext
+        onDragStart={onDragStart}
+        onDragUpdate={onDragUpdate}
+        onDragEnd={onDragEnd}
+      >
+        <div className="source">
+          <h3>包内容窗口</h3>
+          <select defaultValue={selectedVersion} onChange={onSelect}>
+            {VersionOptions}
+          </select>
+          <Droppable droppableId="source">
+            {(provided, snapshot) => (
+              <div
+                ref={provided.innerRef}
+                style={getListStyle(snapshot.isDraggingOver)}
+              >
+                {source.map((item, index) => (
+                  <Draggable
+                    key={item}
+                    draggableId={encode('source', item)}
+                    index={index}
+                  >
+                    {(provided, snapshot) => (
+                      <div
+                        ref={provided.innerRef}
+                        {...provided.draggableProps}
+                        {...provided.dragHandleProps}
+                        style={getItemStyle(
+                          snapshot.isDragging,
+                          provided.draggableProps.style,
+                        )}
+                      >
+                        {item}
+                      </div>
+                    )}
+                  </Draggable>
+                ))}
+                {provided.placeholder}
+              </div>
+            )}
+          </Droppable>
         </div>
         <div>
-          <label>window.</label>
-          <input />
+          <h3>资源窗口</h3>
+          <div>
+            <label>Version Range:</label>
+            <input />
+          </div>
+          <div>
+            <label>window.</label>
+            <input />
+          </div>
+          <h4>Javascript</h4>
+          {JSSolutions}
+          <h4>Css</h4>
+          {CSSSolutions}
         </div>
-        {JSSolutions}
-      </div>
+      </DragDropContext>
       <div>测试窗口</div>
     </div>
   )
